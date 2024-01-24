cd87037ca3bdfdd3eed05cd55c61ccea669f6fc6
Ashutosh Sahu <sahuashutosh248@gmail.com>
2024-01-25 00:48:57 +0530

    Compare both arr

---
+++ FindTheOriginalArrayofPrefixXor.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class FindTheOriginalArrayofPrefixXor {
+    public static int[] findArray(int[] pref) {
+        int xor = 0;
+        int[] result = new int[pref.length];
+        for( int i=0;i<pref.length;i++ ){
+            result[i] = xor^pref[i];
+            xor ^= result[i];
+        }
+        return result;
+    
+    }
+    public static void main(String[] args) {
+        int[] pref = {5};
+        System.out.println(Arrays.toString(findArray(pref)));
+    }
+}
